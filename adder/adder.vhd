a8907923f74d37e75bc09e9fd5e1822d5b0c44d4
Ross Barnowski <rossbar@berkeley.edu>
2017-09-03 17:24:20 -0700

    Changed port names in adder.vhd to match those in adder_tb.
    Not yet sure how mapping of ports works, so took the easy
    way out and just changed names to match the example.

--- adder/adder.vhd
+++ adder/adder.vhd
@@ -1,13 +1,13 @@
 ENTITY adder IS
-    PORT( a: IN BIT;    -- First bit to add
-          b: IN BIT;    -- Second bit for add
-          cin: IN BIT;  -- Carry-in bit
+    PORT( i0: IN BIT;    -- First bit to add
+          i1: IN BIT;    -- Second bit for add
+          ci: IN BIT;  -- Carry-in bit
           co: OUT BIT;  -- Carry-out bit
-          sum: OUT BIT); -- Sum
+          s: OUT BIT); -- Sum
 END ENTITY adder;
 -------------------------------------------------------------------------------
 ARCHITECTURE addition OF adder IS
 BEGIN
-    sum <= a XOR b XOR cin;
-    co <= (a AND b) OR (a AND cin) OR (b AND cin);
+    s <= i0 XOR i1 XOR ci;
+    co <= (i0 AND i1) OR (i0 AND ci) OR (i1 AND ci);
 END ARCHITECTURE addition;
