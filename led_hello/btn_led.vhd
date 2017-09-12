13f3b9bce11fd30b1faefe4b51cd4b20e66c0d45
Ross Barnowski <rossbar@berkeley.edu>
2017-09-12 12:44:14 -0700

    Modified LED example to use tricolor LED.

--- led_hello/btn_led.vhd
+++ led_hello/btn_led.vhd
@@ -3,10 +3,17 @@ USE ieee.std_logic_1164.all;
 -------------------------------------------------------------------------------
 ENTITY btn_led IS
     PORT(btn: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-         led: OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+         led0_r: OUT STD_LOGIC;
+         led0_g: OUT STD_LOGIC;
+         led0_b: OUT STD_LOGIC);
 END ENTITY btn_led;
 -------------------------------------------------------------------------------
 ARCHITECTURE lightup OF btn_led IS
 BEGIN
-    led <= btn;
+    led0_r <= '0' WHEN btn="10" ELSE
+              '1';
+    led0_g <= '0' WHEN btn="01" ELSE
+              '1';
+    led0_b <= '0' WHEN btn="11" ELSE
+              '1';
 END ARCHITECTURE lightup;
