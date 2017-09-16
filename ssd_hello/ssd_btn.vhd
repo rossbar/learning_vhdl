c37cfd0a7e09c0875e1457bde49bd95210668be4
Ross Barnowski <rossbar@berkeley.edu>
2017-09-15 19:38:57 -0700

    Added clock.
    Use clock and last_button_state to try to get
    'on_button_press' type behavior. Works better but not perfect.
    
    Also implemented different functionality for the two different
    buttons (incr, decr)

--- ssd_hello/ssd_btn.vhd
+++ ssd_hello/ssd_btn.vhd
@@ -2,27 +2,36 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 -------------------------------------------------------------------------------
 ENTITY ssd_btn IS
-    PORT(btn: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+    PORT(sysclk: IN STD_LOGIC;
+         btn: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
          ssd: OUT STD_LOGIC_VECTOR(6 DOWNTO 0));
 END ENTITY ssd_btn;
 -------------------------------------------------------------------------------
 ARCHITECTURE show_all OF ssd_btn IS
     SHARED VARIABLE value: INTEGER RANGE 0 TO 9 := 0;
-    SIGNAL val: INTEGER RANGE 0 TO 9 := 0;
+    SIGNAL last_button_state : STD_LOGIC := '0';
 BEGIN
-    button_process: PROCESS (btn)
+    button_process: PROCESS (sysclk)
     BEGIN
-        IF (btn="10") THEN
-            IF (value=9) THEN
-                value := 0;
-            ELSE
-                value := value + 1;
+        IF (rising_edge(sysclk)) THEN
+            IF (btn(1)='1' AND last_button_state='0') THEN
+                IF (value=9) THEN
+                    value := 0;
+                ELSE
+                    value := value + 1;
+                END IF;
+            ELSIF (btn(0)='1' AND last_button_state='0') THEN
+                IF (value=0) THEN
+                    value := 9;
+                ELSE
+                    value := value - 1;
+                END IF;
             END IF;
+            last_button_state <= (btn(0) OR btn(1));
         END IF;
     END PROCESS button_process;
-    val <= value;
     ---------------------------
-    WITH val SELECT
+    WITH value SELECT
         ssd <= "0000Z00" WHEN 0,
                "0ZZZZ0Z" WHEN 1,
                "00Z00Z0" WHEN 2,
