8bb7cf993deee8b1240abde5ea739eb0c3d4309a
Ross Barnowski <rossbar@berkeley.edu>
2017-09-15 18:04:49 -0700

    First attempt at driving SSD.
    Counter b/c button_process is NOT an edge only phenomenon.

--- ssd_hello/ssd_btn.vhd
+++ ssd_hello/ssd_btn.vhd
@@ -7,7 +7,31 @@ ENTITY ssd_btn IS
 END ENTITY ssd_btn;
 -------------------------------------------------------------------------------
 ARCHITECTURE show_all OF ssd_btn IS
+    SHARED VARIABLE value: INTEGER RANGE 0 TO 9 := 0;
+    SIGNAL val: INTEGER RANGE 0 TO 9 := 0;
 BEGIN
-    ssd <= "0000000" WHEN btn="10" ELSE
-           "ZZZZZZZ";
+    button_process: PROCESS (btn)
+    BEGIN
+        IF (btn="10") THEN
+            IF (value=9) THEN
+                value := 0;
+            ELSE
+                value := value + 1;
+            END IF;
+        END IF;
+    END PROCESS button_process;
+    val <= value;
+    ---------------------------
+    WITH val SELECT
+        ssd <= "0000Z00" WHEN 0,
+               "0ZZZZ0Z" WHEN 1,
+               "00Z00Z0" WHEN 2,
+               "Z0000Z0" WHEN 3,
+               "ZZ0000Z" WHEN 4,
+               "Z00Z000" WHEN 5,
+               "000Z000" WHEN 6,
+               "ZZ00ZZ0" WHEN 7,
+               "0000000" WHEN 8,
+               "ZZ00000" WHEN 9,
+               "Z0ZZ0Z0" WHEN OTHERS;
 END ARCHITECTURE show_all;
